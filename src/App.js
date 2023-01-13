c0160cdf6817293b5b339b5141dd86951637bfcb
llcseprado <91046919+llcseprado@users.noreply.github.com>
2023-01-13 20:01:50 +0800

    gshj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Expense from './Components/Expense'
+
+
+const expenses = [
+  {id:1,name:"iphone",price:996},
+  {id:1,name:"iphone",price:996},
+  {id:1,name:"iphone",price:996},
+  {id:1,name:"iphone",price:996},
+  {id:1,name:"iphone",price:996}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      {expenses.map((expense) => {
+        return (
+          <Expense key={expense.id} name={expense.name} price={expense.price} />
+      )
+
+      })}
     </div>
   );
 }
